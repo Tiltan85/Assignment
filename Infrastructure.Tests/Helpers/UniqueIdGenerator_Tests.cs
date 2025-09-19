ee1063dbb216ccf97c269146a885a35238dd0efa
Tiltan85 <tiltan85@gmail.com>
2025-09-19 18:20:40 +0200

    UnuqueIdGenerator tested

---
+++ Infrastructure.Tests/Helpers/UniqueIdGenerator_Tests.cs
@@ -0,0 +1,21 @@
+﻿
+using Infrastructure.Helpers;
+
+namespace Infrastructure.Tests.Helpers;
+
+public class UniqueIdGenerator_Tests
+{
+    [Fact]
+    public void Gënerate_ShouldReturnGuidAsString_WhenSuccessful()
+    {
+        // Act
+        // request a GUID from Generate
+        string result = UniqueIdGenerator.Generate();
+
+        //Assert
+        //Check so its not empty
+        Assert.NotEmpty(result);
+        // Try to convert string to GUID
+        Assert.True(Guid.TryParse(result, out _));
+    }
+}
