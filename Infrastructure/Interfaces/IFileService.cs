24df475ad5db48c943403d1e004170bb99d960f4
Tiltan85 <tiltan85@gmail.com>
2025-09-19 17:05:10 +0200

    IFileService and FileResult model created

---
+++ Infrastructure/Interfaces/IFileService.cs
@@ -0,0 +1,10 @@
+﻿
+using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces;
+
+public interface IFileService
+{
+    FileResult SaveContentToFile(string path, string content);
+    FileResult GetContentFromFile(string path);
+}
---
+++ Infrastructure/Models/FileResult.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Infrastructure.Models;
+
+public class FileResult
+{
+    public bool Succeeded { get; set; }
+    public string? Error { get; set; }
+    public string? Content { get; set; }
+}
